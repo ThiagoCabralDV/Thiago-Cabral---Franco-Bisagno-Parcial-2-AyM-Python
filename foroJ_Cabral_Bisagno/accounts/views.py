00f8764e76821521dba671fd1aaa4e2e0504404b
ThiagoCabral501 <thiago.cabral@davinci.edu.ar>
2025-11-13 02:37:38 -0300

    Login y registro funcional Mi perfil (TEST) creado
    Login y registro funcional creado, se modifico el navbar para bienvenida de usuario y boton de mi perfil y cerrar sesion al tener una sesion de usuario activa. Pestaña de mi perfil de prueba creada.

--- foroJ_Cabral_Bisagno/accounts/views.py
+++ foroJ_Cabral_Bisagno/accounts/views.py
@@ -2,13 +2,21 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 def registro(request):
     if request.method == "POST":
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm_password')
 
+        # Verificar si las contraseñas coinciden
+        if password != confirm_password:
+            messages.error(request, "Las contraseñas no coinciden.")
+            return redirect('register')
+
+        # Verificar si el usuario ya existe
         if User.objects.filter(username=username).exists():
             messages.error(request, "Ese usuario ya existe.")
             return redirect('registro')
@@ -40,3 +48,8 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('home')
+
+
+@login_required
+def perfil(request):
+    return render(request, 'accounts/perfil.html')
